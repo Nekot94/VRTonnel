5a84a30286874db1a6a102ac5f7959088ee7ec2f
yadrena <19557355+yadrena@users.noreply.github.com>
2020-03-07 20:42:41 +0300

    07

--- components/change-region.js
+++ components/change-region.js
@@ -1,46 +1,66 @@
 AFRAME.registerComponent('change-region', {
     schema: {
-      map: {type: 'selector'},
+      map: {type: 'selector', default:"#map"},
       number: { default: 1 }
     
     },
 
     init: function () {
         let data = this.data;
+        let el = this.el;
         let map = this.data.map;
         let number = data.number;
         let currAttribute = map.getAttribute("src");
         var rightPanels = [];
+        var points = document.getElementsByClassName("point");
+        
         for(let i=0; i<3; i++)
         {
             rightPanels.push(document.getElementById("r"+i));
         }
-        console.log(rightPanels);
+
+
 
         this.el.addEventListener("mouseenter", evt => {
-            // if (map.getAttribute("src") == "#map" + number)
-            //     return;
+
+            currAttribute = map.getAttribute("src");
             map.setAttribute("src","#map"+number);
-            map.emit("up");
+            // map.emit("up");
+            el.emit("up");
         });
 
         
         this.el.addEventListener("mouseleave", evt => {
             map.setAttribute("src", currAttribute);
-            map.emit("down");
+            // map.emit("down");
+            el.emit("down");
         });
 
         this.el.addEventListener("click", evt => {
-            console.log("c", currAttribute);
-            // if (map.getAttribute("src") == "#map" + number)
-            //     return;
+
             map.setAttribute("src", "#map"+number);
+            // el.setAttribute("src", "#pointn");
             currAttribute = "#map" + number;
-            map.emit("fastdown");
-            rightPanels.forEach(e => {
-                e.emit("tozero");
-                e.emit("scaleev");
-            });
+            // map.emit("fastdown");
+            el.emit("fastdown");
+            for(let i=0; i<3; i++)
+            {
+                rightPanels[i].emit("tozero");
+                rightPanels[i].emit("scaleev");
+                rightPanels[i].setAttribute("src", "#r"+ i + number);
+            }
+
+            for(let i=0; i<points.length; i++)
+            {
+                if (i == number - 1)
+                {
+                    points[i].setAttribute("material", "src", "#pointa");
+                    // points[i].classList.remove("interractible");
+                    continue;
+                }
+                points[i].setAttribute("material", "src", "#pointn");
+                // points[i].classList.add("interractible");
+            }
         
         });
     
